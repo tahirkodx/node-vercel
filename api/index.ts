175baa18312ac15fd8e900fdaded5d8c82aade83
tahirkodx <tahir@kodxsystem.com>
2024-03-26 22:25:42 +0500

    Youtube id attached

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,10 @@
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => res.send("Express on Vercel"));
-// const PORT = 3001
-// app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
-
-// module.exports = app;
-
 require("dotenv").config();
 
 const express = require("express");
-// const dataRoutes = require("./routes/dataRoutes");
 const app = express();
 const youtubeApi = require("./routes/youtube");
 const cors = require("cors")
 const PORT = process.env.PORT || 3000;
-// const apiKey = process.env.YOUTUBE_API_KEY || 'AIzaSyDuP-DCnFOUqE0whVUCgC6Fg2c1T7zlan8';
 
 app.use(cors());
 // app.use("/api", dataRoutes);
@@ -25,25 +14,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/title", async (req, res) => {
-    res.send({message: "/api/title"})
-//   try{
-//   const { videoId } = req.query;
-//   const response = await youtube.getYoutubeSearch(videoId)
-//   res.send({ status: true, message: "Youtube video.", data: response });
-//   } catch(e){
-//     res.send({message: "Internal Server Error!", error: e})
-//   }
+  try{
+  const { videoId } = req.query;
+  const response = await youtubeApi.getYoutubeSearch(videoId)
+  res.send({ status: true, message: "Youtube video.", data: response });
+  } catch(e){
+    res.send({message: "Internal Server Error!", error: e})
+  }
 });
 
 app.get("/api/comments", async (req, res) => {
-    res.send({message: "/api/comments"})
-//   try{
-//   const { videoId } = req.query;
-//   const response = await youtube.getCommentsThreads(videoId)
-//   res.send({ status: true, message: "Youtube comments.", data: response });
-//   } catch(e){
-//     res.send({message: "Internal Server Error!", error: e})
-//   }
+  try{
+  const { videoId } = req.query;
+  const response = await youtubeApi.getCommentsThreads(videoId)
+  res.send({ status: true, message: "Youtube comments.", data: response });
+  } catch(e){
+    res.send({message: "Internal Server Error!", error: e})
+  }
 });
 
 app.listen(PORT, () => {
