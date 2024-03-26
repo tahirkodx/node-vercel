9a3d79097dc9e44c4303d1538df8051dfa04c633
tahirkodx <tahir@kodxsystem.com>
2024-03-26 22:13:00 +0500

    initial commit of the project

---
+++ api/index.ts
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+const PORT = 3001
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+
+module.exports = app;
